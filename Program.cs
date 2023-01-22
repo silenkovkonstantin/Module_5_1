a3db2682d787c2daadfaa9254472df5b05de56bf
silenkovkonstantin <silenkov@inbox.ru>
2023-01-22 12:41:49 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var array = GetArrayFromConsole();
+    }
+
+    static int[] GetArrayFromConsole()
+    {
+        var result = new int[5];
+
+        for (int i = 0; i < result.Length; i++)
+        {
+            Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+            result[i] = int.Parse(Console.ReadLine());
+        }
+
+        int temp = 0;
+        for (int i = 0; i < result.Length; i++)
+        {
+            for (int j = 0; j < result.Length; j++)
+            {
+                if (result[i] < result[j])
+                {
+                    temp = result[i];
+                    result[i] = result[j];
+                    result[j] = temp;
+                }
+            }
+            
+        }
+        foreach (int item in result)
+        {
+            Console.WriteLine(item);
+        }
+
+        return result;
+    }
+}
